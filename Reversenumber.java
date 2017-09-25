6802dc79c6278118abdd6d48ac60da7457ab4439
hema29 <31930095+hema29@users.noreply.github.com>
2017-09-26 00:06:38 +0530

    Add files via upload

---
+++ Reversenumber.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class JavaProgram
+{
+    public static void main(String args[])
+    {
+        int num, rev=0, rem;
+        Scanner scan = new Scanner(System.in);
+		
+        System.out.print("Enter a Number : ");
+        num = scan.nextInt();
+		
+        while(num != 0)
+        {
+            rem = num%10;
+            rev = rev*10 + rem;
+            num = num/10;
+        }
+		
+        System.out.print("Reverse = " +rev);
+    }
+}
